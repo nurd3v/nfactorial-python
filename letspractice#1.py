b6e75be557e6c3ef7a31ae5dfac24d8861d91dc9
nurd3v <ukimetov.nurtegin@gmail.com>
2026-08-28 05:31:49 +0500

    lets_practice

---
+++ letspractice#1.py
@@ -0,0 +1,24 @@
+#Create two variables a = 5 and b = 10. Swap their values so that a becomes 10 and b becomes 5
+a = 5
+b = 10
+
+temp = a
+a = b
+b = temp
+
+print(a, b)
+
+
+#Make two variables: a = 2 and b = 3. Create a third variable c that stores their sum
+
+c = 2
+d = 3
+f = c + d
+print(f)
+
+#Start with a = 4 and b = 6. Make a variable is_smaller that is True if a is less than b
+
+e = 4
+k = 6
+g = (a > k)
+print(g)
