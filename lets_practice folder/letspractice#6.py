bfa4260541eacda357b93ed01cb9bfc9846b6621
nurd3v <ukimetov.nurtegin@gmail.com>
2026-08-31 21:31:27 +0500

    -

---
+++ lets_practice folder/letspractice#6.py
@@ -0,0 +1,5 @@
+#3. Ask the user for a number. Print its square root, but rounded **down** to the nearest integer.
+import math
+
+a = int(input("Enter your number: "))
+print((math.floor(math.sqrt(a))))
