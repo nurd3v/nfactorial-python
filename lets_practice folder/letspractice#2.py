37b82a2ac8afc07bdf56f20844d9e4426b279371
nurd3v <ukimetov.nurtegin@gmail.com>
2026-08-31 20:45:30 +0500

    lets practice

---
+++ lets_practice folder/letspractice#2.py
@@ -0,0 +1,6 @@
+#Условие: Ввести имя и возраст, вывести сообщение с возрастом через 5 лет.
+
+name = input("Enter your name: ")
+age = input("Enter you age: ")
+ 
+print("My name is" + " " + name + "." " " + "I am" + " "+ age + " " +"years old." + " " + "And after 5 years, i am gonna be senior backend developer.")
---
+++ letspractice#3.py
@@ -0,0 +1,12 @@
+#Условие: Даны три числа a, b, c. Вывести True, если a < b и b > c, иначе False.
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a < b and b > c:
+    print("True")  
+else: 
+    print ("False")
+    
+        
