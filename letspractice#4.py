602e7f12129e858b5cb0d67271c4571c2d703983
nurd3v <ukimetov.nurtegin@gmail.com>
2026-08-31 21:25:05 +0500

    .

---
+++ letspractice#4.py
@@ -0,0 +1,7 @@
+#Ask the user for three numbers. Print the smallest and the largest one.
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+print(max(a, b, c))
---
+++ letspractice#5.py
@@ -0,0 +1,6 @@
+#Ask the user for two numbers. Print the absolute difference between them.
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print(a-b)
