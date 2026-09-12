49befdcc5c2d2537847724e8b1f48c2b053eeca9
nurd3v <ukimetov.nurtegin@gmail.com>
2026-09-12 20:52:35 +0500

    asdd

---
+++ 6.py
@@ -0,0 +1,7 @@
+# Пользователь вводит числа. Цикл должен продолжаться, пока пользователь не введёт 0.
+while True:
+    i = int(input("Enter your number: "))
+    print (i)
+    if i == 0:
+        break
+
