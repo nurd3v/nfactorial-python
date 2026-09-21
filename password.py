037415e1297c46364c21508026bf1c25fa9bd974
nurd3v <ukimetov.nurtegin@gmail.com>
2026-09-21 17:20:24 +0500

    asdad

--- password.py
+++ password.py
@@ -2,15 +2,13 @@ chance = 0
 attempt = 3
 while chance < 3:
     password = input("Enter your password: ")
-    while True:
-        if password == "Nfac129!_":
-            print("Correct password!")
-            break
-        elif password != "Nfac129!_":
-            attempt -= 1
-            print(f"You have {attempt} chance")
-            if attempt == 0:
-                print("Sorry you spilled your chance")
-            break   
-       
+    if password == "Nfac129!_":
+        print("Correct answer!")
+        break
+    if password != "Nfac129!_":
+        attempt -= 1
+        print(f"You have {attempt} chance")
+        if attempt == 0:
+            print("Sorry, you have spilled your full chance")
+            break    
     chance += 1
